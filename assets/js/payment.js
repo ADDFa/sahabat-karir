290a76741629e682ea1865f1bc589bad1b171942
ADDFa <fratamaadha18@gmail.com>
2022-05-11 14:09:15 +0700

    Membuat halaman pilih pembayaran | Desktop mode

--- assets/js/payment.js
+++ assets/js/payment.js
@@ -15,12 +15,12 @@ function Rp(nilai) {
 
 // TODO: Total
 const pSubTotal = [...document.querySelectorAll('p.sub-total')]
-const pTotalTagihan = document.querySelector('p.total-tagihan')
+const pTotalTagihan = [...document.querySelectorAll('p.total-tagihan')]
 const harga = [...document.querySelectorAll('span.harga')]
 let biayaAdmin = document.querySelector('span.biaya-admin').innerText
 biayaAdmin = parseInt(biayaAdmin.split('.').join(''))
 
-const total = harga.map((e) => {
+let total = harga.map((e) => {
     let harga = (e.innerText).split('.')
     harga = harga.join('')
     harga = parseInt(harga)
@@ -28,18 +28,47 @@ const total = harga.map((e) => {
     return harga
 }).reduce((accu, curr) => accu + curr)
 
-const totalTagihan = total + biayaAdmin
+const hargaPaket = [...document.querySelectorAll('.hargaPaket')]
 
-// TODO: Masukkan Total kedalam element pTotal
-pSubTotal.map(e => e.innerText = `Rp. ${Rp(total)}`)
+hargaPaket.forEach(e => {
+    e.addEventListener('click', e => {
+        let target = e.target.value
+        target = target.split('.').join('')
+        target = parseInt(target);
 
-// TODO: Masukkan Total Tagihan kedalam element pTotalTagihan
-pTotalTagihan.innerText = `Rp. ${Rp(totalTagihan)}`
+        (e.target.checked) ? total += target: total -= target;
 
-document.querySelector('input').addEventListener('input', e => {
-    if (e.target.checked == true) {
-        console.log('sip')
-    } else {
-        console.log('pis')
-    }
+        setHarga(total)
+    })
+})
+
+window.onload = setHarga(total)
+
+// TODO: Function untuk memasukkan harga kedalam html
+
+function setHarga(total) {
+    const totalTagihan = total + biayaAdmin
+
+    // TODO: Masukkan Total kedalam element pTotal
+    pSubTotal.map(e => e.innerText = `Rp. ${Rp(total)}`)
+
+    // TODO: Masukkan Total Tagihan kedalam element pTotalTagihan
+    pTotalTagihan.map(e => e.innerText = `Rp. ${Rp(totalTagihan)}`)
+
+    document.getElementById('biaya-admin').innerText = `Rp. ${Rp(biayaAdmin)}`
+    document.getElementById('kirim').setAttribute('value', totalTagihan)
+}
+
+// TODO: Saat tombol pilih pembayaran di klik
+const pilihPembayaran = document.querySelector('.pilih-pembayaran')
+const tombolClose = document.querySelector('.tombol-close')
+
+pilihPembayaran.addEventListener('click', () => {
+    document.querySelector('.bayar').classList.add('popup-bayar')
+})
+
+tombolClose.addEventListener('click', e => {
+    e.preventDefault()
+
+    document.querySelector('.bayar').classList.remove('popup-bayar')
 })
